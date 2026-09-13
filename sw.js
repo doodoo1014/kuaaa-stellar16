17b1ccb5443639884a12ca256a2c010a9fcc9b1d
doodoo1014 <49575899+doodoo1014@users.noreply.github.com>
2026-09-14 05:48:35 +0900

    Bump cache for v7.8 fixes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='stellar16-v7.7';
+const CACHE='stellar16-v7.8';
 const LOCAL=['./','./index.html','./styles.css?v=7.7','./hotfix-v77.css?v=7.7','./app.js?v=7.7','./hotfix-v76.js?v=7.7','./hotfix-v77.js?v=7.7','./manifest.webmanifest','./data/western-stellar16.json','./data/stars-stellar16.csv'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(LOCAL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
