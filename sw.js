675f9ed39de261f478faf9430f20e2d4220a2b71
doodoo1014 <49575899+doodoo1014@users.noreply.github.com>
2026-09-14 04:42:04 +0900

    Bump service worker cache for v7.2 deployment

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='stellar16-v7.1';
+const CACHE='stellar16-v7.2';
 const LOCAL=['./','./index.html','./styles.css','./app.js','./save-image-v2.js','./manifest.webmanifest','./data/western-stellar16.json','./data/stars-stellar16.csv'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(LOCAL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
